3dededc3fa510b7cc9b665824dd37f3c4eed6aaf
nosyarg <york17a@ncssm.edu>
2016-05-14 11:49:39 -0400

    set up git lfs to track files with nwk (network) extension

--- erdosrenyi.py
+++ erdosrenyi.py
@@ -1,7 +1,7 @@
 import random
 n = int(input("Enter number of vertices: "))
 p = float(input("Enter edge probability: "))
-writefile = open("erdosgraph.txt","w")
+writefile = open("erdosgraph.nwk","w")
 graph = []
 for i in range(0,n):
         for j in range(i+1,n):
