1774ad6fe73219c0f4cec07d320da79fa095382e
nosyarg <york17a@ncssm.edu>
2016-05-14 20:34:51 -0400

    got the graph cleaning script working

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# networksfinalproject
+#A Generalization of The Knapsack Problem to Routing on Networks
+Instructions for use:
+1. add graph to working directory, or generate a graph using `erdosrenyi.py`
+2. run clean graph on the `.nwk` file to save only the giant component
+3. now the fun begins...
--- cleangraph.py
+++ cleangraph.py
@@ -4,6 +4,49 @@ with open(path) as readfile:
     for line in readfile:
             temparr = line.split(' ')
             graph.append((int(temparr[0]),int(temparr[1])))
+graphtmp = graph[:]
 components = []
-while (graph != []):
-        currentcomp = []
+currentcomponent = []
+while(len(graphtmp) != 0):
+        index = 0
+        currentcomponent.append(graphtmp[0][0])
+        currentcomponenttmp = currentcomponent[:]
+        while(index < len(currentcomponent)):
+                if(currentcomponenttmp != currentcomponent):
+                        index = 0
+                i = 0
+                currentcomponenttmp = currentcomponent[:]
+                while(i < len(graphtmp)):
+                        currentedge = graphtmp[i]
+                        if(currentedge[0] == currentcomponent[index] and not currentedge[1] in currentcomponent):
+                                currentcomponent.append(currentedge[1])
+                                del graphtmp[i]
+                        elif(currentedge[1] == currentcomponent[index] and not currentedge[0] in currentcomponent):
+                                currentcomponent.append(currentedge[0])
+                                del graphtmp[i]
+                        elif(currentedge[0] in currentcomponent and currentedge[1] in currentcomponent):
+                                del graphtmp[i]
+                        else:
+                                i += 1
+                index += 1
+        components.append(sorted(currentcomponent[:]))
+        currentcomponent = []
+lens = []
+for i in components:
+        lens.append(len(i))
+gc = components[lens.index(max(lens))]
+i = 0;
+while(i < len(graph)):
+        currentedge = graph[i]
+        if( not (currentedge[0] in gc or currentedge[1] in gc)):
+                del graph[i]
+        else:
+                i+=1
+print(graph)
+print(gc)
+writestr = ""
+writefile = open("cleanedgraph.nwk","w")
+for i in range(len(graph)):
+        writestr += ' '.join(map(str,(graph[i]))) + "\n"
+writefile.write(writestr)
+writefile.close()
