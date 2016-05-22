391e139cdc30f6d6cde20f3846f126d6831887bc
nosyarg <york17a@ncssm.edu>
2016-05-21 23:22:43 -0400

    Got the preprocessing to work completely

--- combinedreduce.py
+++ combinedreduce.py
@@ -1,29 +1,78 @@
+#NETWORKS FINAL PROJECT, MINIMUM COST MAXIMUM SUCCESS ROUTING OVER NETWORKS
+#GRAYSON YORK, LUKE ZELLER, MATTHEW ZHENG, JUNMO RYANG
+#5/25/2016
 import random
+def uselessfunctionthatIwanttokeep():
+        indegs = indeg(graph)
+        for index,val in enumerate(indegs):
+                if val == 0: #cut sources
+                        j = 0 
+                        while j<len(graph):
+                                current = graph[j]
+                                if((current[0] == index or current[1] == index) and not (current[1] in exempt or current[0] in exempt)):
+                                        del graph[j]
+                                        print("srcrm")
+                                else:
+                                        j+=1
+        outdegs = outdeg(graph)
+        for index,val in enumerate(outdegs):
+                if val == 0: #cut sinks
+                        j = 0 
+                        while j<len(graph):
+                                current = graph[j]
+                                if((current[0] == index or current[1] == index) and not (current[1] in exempt or current[0] in exempt)):
+                                        del graph[j]
+                                        print("snkrm")
+                                else:
+                                        j+=1
+def maxnum(graph):
+        maxval = 0
+        for edge in graph:
+                if edge[1] > maxval:
+                        maxval = edge[1]
+        return maxval
+def degree(graph):
+        maxval = maxnum(graph)
+        #compute all of the degrees of vertices
+        degs = [0] * (maxval+1)
+        for edge in graph:
+                degs[edge[0]] += 1
+                degs[edge[1]] += 1
+        return degs
+def outdeg(graph):
+        maxval = maxnum(graph)
+        #compute all of the degrees of vertices
+        degs = [0] * (maxval+1)
+        for edge in graph:
+                degs[edge[0]] += 1
+        return degs
+def indeg(graph):
+        maxval = maxnum(graph)
+        #compute all of the degrees of vertices
+        degs = [0] * (maxval+1)
+        for edge in graph:
+                degs[edge[1]] += 1
+        return degs
 def probdist(edge): #insert probability distribution function
         return random.random()
 def costdist(edge): #insert cost distribution function
         return random.random()
 def graphgen():#insert graph generation function which should append the cost and probabilities
         n = 1000
-        p = .01
+        p = .1
         graph = []
         for i in range(0,n):
                 for j in range(i+1,n):
                         if (random.random() < p): 
                                 graph.append((i,j,probdist((i,j)),costdist((i,j))))
+        print(graph)
+        print(degree(graph))
         return graph
-#def acyclic(graph):
-#this should remove cycles from the graph, probably easiest to do by ensuring that all destinations are less than origins
+def acyclic(graph):
+        return graph
+#this should eventually remove cycles from the graph, probably easiest to do by ensuring that all destinations are less than origins
 def removezero(graph):
-#now I will compute all of the degrees of vertices
-        maxval = 0
-        for edge in graph:
-                if edge[1] > maxval:
-                        maxval = edge[1]
-        degs = [0] * (maxval+1)
-        for edge in graph:
-                degs[edge[0]] += 1
-                degs[edge[1]] += 1
+        degs = degree(graph)
         #now go through the degrees, and rewire to lower numbers when zero 
         newindices = {}
         zerodegs = []
@@ -39,6 +88,60 @@ def removezero(graph):
                 graph[index] = (newindices[edge[0]],newindices[edge[1]],edge[2],edge[3])
         return graph
         #this function should reduce the graph to remove disconnected edges 
+def cutirrelevant(graph,exempt):
+        degs = degree(graph)
+        for index,val in enumerate(degs):
+                if val == 1: #cut degree 1 vertices
+                        j = 0 
+                        while j<len(graph):
+                                current = graph[j]
+                                if((current[0] == index or current[1] == index) and not (current[1] in exempt or current[0] in exempt)):
+                                        del graph[j]
+                                        j = len(graph)
+                                        print("1rm")
+                                else:
+                                        j+=1 #todo, cut out sink vertices and source vertices and simplify degree 2's
+        degs = degree(graph)
+        index = 0
+        while(index < len(degs)):
+                degs = degree(graph)
+                val = degs[index]
+                if val == 2: #simplify degree 2 vertices this is malfunctioning, zero has way to high of degree and there are still too many degree 2 vertices
+                        j = 0
+                        while j < len(graph):
+                                first = graph[j]
+                                if (first[1]  == index) and not (first[0] in exempt or first[1] in exempt):
+                                        i = j+1
+                                        while i < len(graph):
+                                                second = graph[i]
+                                                if (second[0] == index) and not (second[0] in exempt or second[1] in exempt):
+                                                        new = (first[0],second[1],first[2]*second[2],first[3]+second[3])#make a new edge which is a combination of the other two
+                                                        graph[j] = new
+                                                        del graph[i]
+                                                        i = len(graph)
+                                                        j = len(graph)
+                                                        index = len(degs)
+                                                        print("2rm")
+                                                i += 1
+                                j += 1
+                index+=1
+#        print(degree(graph))
+#        print(indeg(graph))
+#        print(outdeg(graph))
+        hasin = [0]*len(degree(graph))
+        hasout = [0]*len(degree(graph))
+        for val in graph:
+                hasout[val[0]] = 1 or hasout[val[0]]
+                hasin[val[1]] = 1 or hasin[val[1]]
+        i = 0
+        while i < len(graph):
+                val = graph[i]
+                if (not hasin[val[0]] and not val[0] in exempt) or (not hasin[val[1]] and not val[1] in exempt):
+                        del graph[i]
+                else:
+                        i+=1
+        return graph
+#BEGIN PROGRAM
 graph = graphgen()
 print("GRAPH GENERATED, E = " + str(len(graph)))
 graphtmp = graph[:]
@@ -80,5 +183,27 @@ while(i < len(graph)):#delete all vertices not in the gc
         else:
                 i+=1
 graph = removezero(graph)
+vertinpath = [0]
+current = 0
+for i in graph:
+                if i[0] in vertinpath and not i[1] in vertinpath:
+                        vertinpath.append(i[1])
+i = 0
+while(i < len(graph)):
+        current = graph[i]
+        if not current[0] in vertinpath:
+                del graph[i]
+        else:
+                i += 1
+graph = removezero(graph)
 print("GC isolated, E = " + str(len(graph)))
-
+print(graph)
+graphtmp = []
+while(graph != graphtmp):#get rid of all the vertices we know we wont pass through
+        graphtmp = graph[:]
+        start = 0
+        end = maxnum(graph)
+        graph = removezero(cutirrelevant(graph,[start,end]))
+print("Irrelevant vertices cut, E = " + str(len(graph)))
+print(graph)
+#now lets get into the code...
